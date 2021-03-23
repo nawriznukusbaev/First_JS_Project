a38f8b4c739d27871bd4a6b657eeb5bbdbe8d7a8
Nawriz <nawriznukusbaev@gmail.com>
2021-03-23 23:20:45 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use sctrict";
+
+let number  = 5;
+const LeftBorderWith= 5;
+
+number = 10;
+
+//console.log(90);
+
+let arr=[1,2,3,4];
+
+
+//const answer=+prompt("How do you do?"); 
+const answers = [];
+
+/* answers[0]=prompt("What's you name?","");
+answers[1]=prompt("What's you name whife?","");
+answers[2]=prompt("What's you name Mother?",""); */
+
+//document.write(answers);
+const category="toys";
+
+//console.log(`https://karsu.uz/${category}/7`);
+console.log(2*4===8);
