9a7e6572eda09b515661f0c647f4efe43480a7c4
Nawriz <nawriznukusbaev@gmail.com>
2021-03-25 00:37:28 +0500

    practis #1

--- script.js
+++ script.js
@@ -21,4 +21,23 @@ answers[2]=prompt("What's you name Mother?",""); */
 const category="toys";
 
 //console.log(`https://karsu.uz/${category}/7`);
-console.log(2*4===8);
+//console.log(2*4===8);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+console.log(numberOfFilms);
+
+const personalMovieDB={
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:{},
+    privat:false
+};
+
+const a=prompt("Один из последних просмотренных фильмов?",""),
+      b=prompt("На сколько оцените его?",""),
+      c=prompt("Один из последних просмотренных фильмов?",""),
+      d=prompt("На сколько оцените его?","");
+
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+      console.log(personalMovieDB);
