584b29986d03ef0210b579ea8664da160c880119
Nawriz <nawriznukusbaev@gmail.com>
2021-03-29 01:44:15 +0500

    Practice with object #4

---
+++ function_script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/* function callbac(lang, callback)
+    {
+        console.log(`Я учу ${lang}`);
+        callback();
+    }
+
+function done()
+    {
+        console.log("Урра я прошел этот курс!");
+    }    
+
+callbac("Java Script",done); */
+const student=
+    {
+        firstname:Nawriz,
+        lastname:Nukusbaev,
+        width:68 kg,
+        height: 1.68,
+        special:{language: russian, univer:KSU};
+    };
+
+console.log(student);    
